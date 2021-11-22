6aa86133d347088ed3fa4398ec4fa7c1226e34f8
Giuseppe0197 <faro.g@hotmail.it>
2021-11-22 14:45:39 +0100

    generazione mail tutte insieme

--- js/myscript.js
+++ js/myscript.js
@@ -2,10 +2,50 @@
 
 /* clleghiamo il container di html in vue */
 
-/* creiamo un data con valore vuoto per la mail */
+var containerMail = new Vue (
 
-/* creiamo una array vuota che verrà riempita da un ciclo con axios */
+    {
 
-/* creiamo una variabile self per prendere il this della mail e dell'array */
+        el: "#container",
 
-/* creiamo un ciclo per generare le mail e pushamo il tutto nell'array */
+        data: {
+
+            /* creiamo un data con valore vuoto per la mail */
+
+            mailGenerated: "",
+
+            /* creiamo una array vuota che verrà riempita da un ciclo con axios */
+
+            mailList: []
+
+        },
+
+        mounted() {
+
+            /* creiamo una variabile self per prendere il this della mail e dell'array */
+
+            const self = this;
+
+            /* creiamo un ciclo per generare le mail e pushamo il tutto nell'array */
+
+            for(let i = 0; i < 10; i++){
+
+                axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then(function(mailRandom){
+
+                    self.mailGenerated = mailRandom.data.response;
+
+                    self.mailList.push(self.mailGenerated);
+
+                });
+
+            }
+
+            console.log(self.mailList);
+
+        },
+
+    }
+
+)
