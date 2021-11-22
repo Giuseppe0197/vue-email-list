0e8613428ecea3c2f1fab6fea85e3d937f16fe43
Giuseppe0197 <faro.g@hotmail.it>
2021-11-22 14:17:33 +0100

    impostazione lavoro

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+/* Attraverso l’apposita API di Boolean generare 10 indirizzi email */
+
+/* clleghiamo il container di html in vue */
+
+/* creiamo un data con valore vuoto per la mail */
+
+/* creiamo una array vuota che verrà riempita da un ciclo con axios */
+
+/* creiamo una variabile self per prendere il this della mail e dell'array */
+
+/* creiamo un ciclo per generare le mail e pushamo il tutto nell'array */
